9c2ec1408883ba96e5e89a33909a47ead9711463
Adrian-Montemayor <adrianmoncar@hotmail.com>
2016-08-15 12:01:57 -0500

    New row added

--- readerFunction.py
+++ readerFunction.py
@@ -5,15 +5,26 @@ import csv
 reader = csv.reader(open('SacramentocrimeJanuary2006.csv','rU'))
 
 #Rutina de chequeo x's
+i=0
+arrayDatos = []
 if reader > 1:
     print 
 else:
     print  "Algo salio mal al conectar con el archivo"
-
-
+i = 0;
 for row in reader:
+    if i > 0:    
+        print  "Beat: "+ row[3] 
+        break
+    else:
+        print row
+        i += 1
 
-    print  "Beat: "+ row[3] + "Grid: " + row[4] 
-    print "\n"
+
+
+
+    #+ "Grid: " + row[4] 
+    #print "\n"
 
 #Verificando los nuevos branch 
+
