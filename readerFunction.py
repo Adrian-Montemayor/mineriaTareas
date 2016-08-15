8609c7d480ac7b0496267c843edde91706b8503f
Adrian-Montemayor <adrianmoncar@hotmail.com>
2016-08-15 12:04:44 -0500

    Some items deleted

--- readerFunction.py
+++ readerFunction.py
@@ -4,14 +4,15 @@ import csv
 #Open a CSV FILE
 reader = csv.reader(open('SacramentocrimeJanuary2006.csv','rU'))
 
-#Rutina de chequeo x's
 i=0
-arrayDatos = []
+
+#Rutina de chequeo x's
 if reader > 1:
     print 
 else:
     print  "Algo salio mal al conectar con el archivo"
 i = 0;
+#Recorrer el archivo csv
 for row in reader:
     if i > 0:    
         print  "Beat: "+ row[3] 
