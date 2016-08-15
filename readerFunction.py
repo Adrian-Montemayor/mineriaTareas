7bd976a6eb68e736cdc154321f3db675da7402e3
Adrian-Montemayor <adrianmoncar@hotmail.com>
2016-08-14 21:07:29 -0500

    Read a CSV file

---
+++ readerFunction.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python 
+import csv 
+
+#Open a CSV FILE
+reader = csv.reader(open('SacramentocrimeJanuary2006.csv','rU'))
+
+#Rutina de chequeo x's
+if reader > 1:
+    print 
+else:
+    print  "Algo salio mal al conectar con el archivo"
+
+
+for index,row in enumerate(reader):
+
+    print  "Beat: "+ row[3] + "Grid: " + row[4] 
+    print "\n"
