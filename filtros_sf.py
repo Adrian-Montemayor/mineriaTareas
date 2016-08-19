8922bfc0299a5ed91c38e2865a93a0508d6c7659
ramirouvalle <ramiro.uvallevgr@uanl.edu.mx>
2016-08-19 17:06:59 -0500

    Actualizacion de validaciones e impresion de mensajes

--- filtros_sf.py
+++ filtros_sf.py
@@ -13,42 +13,41 @@ def readFile(fileName):
 				dateLastRow = lista[len(lista)-1][2]
 
 				#PERIODO (INPUT)
-				
+				print "Rango de fechas (" + str(dateFirstRow) +" - "+ str(dateLastRow)+")."
 				while True:
 					try:
-						firstDateSelect = str(input("Fecha inicial: "))
-						lastDateSelect = str(input("Fecha Final:"))
-						firstDateSelect = int(firstDateSelect)
-						lastDateSelect = int(lastDateSelect)						
-						break		
-					except:
+						firstDateSelect = input("Fecha inicial: ")
+						lastDateSelect = input("Fecha Final: ")
+						#Validar que las fecha inicial no sea mayor ala final
+						if firstDateSelect > lastDateSelect:
+							os.system("cls")
+							print "La fecha inicial no puede ser mayor que la final. Intente nuevamente."	
+						else:
+							if (firstDateSelect >= int(dateFirstRow)) and (lastDateSelect <= int(dateLastRow)):
+								break
+							else:
+								os.system("cls")
+								print "Las fechas estan fuera del rango (" + str(dateFirstRow) +" - "+ str(dateLastRow)+"). Intente nuevamente."
+					except Exception as e:
 						os.system("cls")
-						print "Ingrese los anios correctamente (puro numero y dentro del rango).."
+						print "Ingrese los anios correctamente (Ej: 1994)."
 
 				firstDateSelect = str(firstDateSelect)
 				lastDateSelect = str(lastDateSelect)
-				#Si no se pasa ningun valor
-				if firstDateSelect == "" or lastDateSelect == "":
-					firstDateSelect = dateFirstRow
-					lastDateSelect = dateLastRow
-
-				#Validar que las fecha inicial no sea mayor ala final
-				if firstDateSelect > lastDateSelect:
-					print "La fecha inicial no puede ser mayor que la final."	
-					return
-
-				#RAZA DE LA VICTIMA Y DEL SOSPECHOSO (INPUT) y validación
-				os.system("cls")
+				#RAZA DE LA VICTIMA Y DEL SOSPECHOSO (INPUT) y validacion
 				while True:
 					try:
-						victimRace = str(input("Raza de la victica (0, 1, 2, 3, 4, 5, 6):"))
-						suspectRace = str(input("Raza del sospechoso (0, 1, 2, 3, 4, 5, 6):"))
-						victimRace = int (victimRace)
-						suspectRace = int(victimRace)
-						break
+						os.system("cls")
+						victimRace = input("Raza de la victica (0, 1, 2, 3, 4, 5, 6): ")
+						suspectRace = input("Raza del sospechoso (0, 1, 2, 3, 4, 5, 6): ")
+						if (victimRace >= 0 and victimRace <= 6) and (suspectRace >= 0 and suspectRace <= 6):
+							break
+						else:
+							os.system("cls")
+							print "Eliga una raza dentro del rango."
 					except:
 						os.system("cls")
-						print "ingrese los valores correctamente"
+						print "Ingrese los valores correctamente."
 				#Volvemos a parsear para poder comparar la lista
 				victimRace = str(victimRace)
 				suspectRace = str(suspectRace)
@@ -56,23 +55,31 @@ def readFile(fileName):
 				#GENERO DE LA VICTIMA Y DEL SOSPECHOSO (INPUT)
 				while True:
 					try:
-						victimGenre = str(input("Genero de la victima (0, 1, 2, 3, 5, 6):"))
-						suspectGenre= str(input("Genero del sospechoso (0, 1, 2, 3, 5, 6):"))
-						victimGenre = int(victimGenre)
-						suspectGenre = int(suspectGenre)
-						break
-
+						os.system("cls")
+						victimGenre = input("Genero de la victima (0, 1, 2, 3, 5, 6): ")
+						suspectGenre= input("Genero del sospechoso (0, 1, 2, 3, 5, 6): ")
+						if (victimGenre >= 0 and victimGenre <= 6) and (suspectGenre >= 0 and suspectGenre <= 6):
+							break
+						else:
+							os.system("cls")
+							print "Eliga un genero dentro del rango."
+							
 					except:
 						os.system("cls")
-						print "Ingrese los valores correctamente"
+						print "Ingrese los valores correctamente."
+				#Volvemos a parsear para poder comparar la lista
 				victimGenre = str(victimGenre)
 				suspectGenre = str(suspectGenre)
+
 				i=0
 				Vhombres=0
 				Vmujeres=0
 				Shombres=0
 				Smujeres=0
 
+				print "<!===============================================!>"
+				print "Anio inicial: " + firstDateSelect
+				print "Anio final: "+ lastDateSelect
 				for row in range(0,len(lista)):
 					if (lista[row][2] >= firstDateSelect) and (lista[row][2] <= lastDateSelect):
 						if (lista[row][4] == victimRace or victimRace == "0") and (lista[row][9] == suspectRace or suspectRace == "0"):
@@ -86,23 +93,43 @@ def readFile(fileName):
 									Shombres += 1
 								if lista[row][11] == str(2):
 									Smujeres += 1
-								print "Year: " + lista[row][2] + " VRaza: " + lista[row][4] + " SRaza: " + lista[row][9] + " VGenero: " + lista[row][6] + " SGenero: " + lista[row][11] +"\n"
-				print "Total: " + str(i)
-				porcentaje =  int(Vhombres) *  100  / int(i)
-				print "\n#Victimas Hombre: " + str(Vhombres) +"| %" + str(porcentaje)
-				porcentaje =  int(Vmujeres) *  100  / int(i)
-				print "\n#Victimas Mujer: "  + str(Vmujeres) +"| %" + str(porcentaje)
-				restantes =  int(i) - (Vhombres + Vmujeres) 
-				restantes =  int(restantes) * 100 / int(i)
-				print "No definidos: %" + str(restantes)
+								#print "Year: " + lista[row][2] + " VRaza: " + lista[row][4] + " SRaza: " + lista[row][9] + " VGenero: " + lista[row][6] + " SGenero: " + lista[row][11] +"\n"
+								print "[" + lista[row][2] + ", Name: " + lista[row][3] + ", RV: " + lista[row][4] + ", GV: " + lista[row][6] + ", RS: " + lista[row][9] + ", GS: " + lista[row][11] + ", Weapon: " + lista[row][15] +"]"
 
-				porcentaje =  int(Shombres) *  100  / int(i)
-				print "\n#Hombres sospechosos: " + str(Shombres) + "| %" + str(porcentaje)
-				porcentaje = int(Smujeres) * 100 / int(i)
- 				print "\n#Mujeres sospechosos: " + str(Smujeres) + "| %" + str(porcentaje)
- 				restantes =  int(i) - (Shombres + Smujeres) 
-				restantes =  int(restantes) * 100 / int(i)
-				print "No definidos: %" + str(restantes)
+				print "<!===============================================!>"
+				print "Total de registros: " + str(i)
+				try:
+					porcentaje =  float(Vhombres) *  100  / int(i)
+				except:
+					porcentaje = 0
+				print "\nVictimas Hombre: " + str(Vhombres) +" - Porcentaje: %.2f" %porcentaje +"%"
+				try: 
+					porcentaje =  float(Vmujeres) *  100  / int(i)
+				except: 
+					porcentaje = 0
+				print "\nVictimas Mujer: "  + str(Vmujeres) +" - Porcentaje: %.2f" %porcentaje +"%"
+				try:
+					restantes =  int(i) - (Vhombres + Vmujeres) 
+					restantes =  float(restantes) * 100 / int(i)
+				except:
+					restantes = 0
+				print "No definidos: %.2f" %restantes+ "%"
+				try:
+					porcentaje =  float(Shombres) *  100  / int(i)
+				except:
+					porcentaje = 0
+				print "\nHombres sospechosos: " + str(Shombres) + " - Porcentaje: %.2f" %porcentaje +"%"
+				try: 
+					porcentaje = float(Smujeres) * 100 / int(i)
+				except:
+					porcentaje = 0
+ 				print "\nMujeres sospechosos: " + str(Smujeres) + " - Porcentaje: %.2f" %porcentaje +"%"
+ 				try:
+ 					restantes =  int(i) - (Shombres + Smujeres) 
+					restantes =  float(restantes) * 100 / int(i)
+				except:
+					restantes = 0
+				print "No definidos: %.2f" %restantes+ "%"
 
 				if  int(i) == 0:
 					print "No se encontro ni un dato"
